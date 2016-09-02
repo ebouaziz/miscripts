235e74a5f875329e30631c877e3e5e541d060700
Emmanuel Blot <manu.blot@gmail.com>
2016-09-02 20:23:49 +0200

    Create a basic antenna selector

--- Python/ftdi/wftdi.py
+++ Python/ftdi/wftdi.py
@@ -1,18 +1,20 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-"""
-Dependencies:
-  - pyserial (tested with 3.0)
-  - pyusb    (tested with 1.0)
-  - pyftdi   (tested with 0.13.2)
-  - cherrypy (tested with 7.0)
-  - jinja    (tested with 2.8)
-"""
+"""Web antenna selector"""
+
+# Dependencies:
+#   - pyserial (tested with 3.0)
+#   - pyusb    (tested with 1.0)
+#   - pyftdi   (tested with 0.13.2)
+#   - cherrypy (tested with 7.0)
+#   - jinja    (tested with 2.8)
+
 
 import cherrypy
 import os.path
 import sys
+from argparse import ArgumentParser
 from collections import OrderedDict
 from jinja2 import Environment, FileSystemLoader
 from pyftdi.gpio import GpioController, GpioException
@@ -20,55 +22,89 @@ from pyftdi.gpio import GpioController, GpioException
 
 class Root(object):
 
-    PREFIX = 'gpio'
-    PINS = 8
+    PREFIX = 'ant'
+    ANTENNAS = {900: (4,),
+                2450: (16,)}
 
     def __init__(self):
         ldir = os.path.dirname(__file__)
         self.env = Environment(loader=FileSystemLoader(ldir))
-        self._output = 0
+        self._antennas = {'%s%d' % (self.PREFIX, k): 1
+                          for k in self.ANTENNAS}
         self._bailout = False
+        self._ftdis = {k: None for k in self.ANTENNAS}
 
     @cherrypy.expose
     def index(self, k='', v=''):
         if self._bailout:
             sys.exit(0)
-        checked = v == 'true'
-        if k.startswith(self.PREFIX):
-            ix = int(k.split('_', 1)[1])
-            if checked:
-                self._output |= (1 << ix)
-            else:
-                self._output &= ~(1 << ix)
-        elif k == 'restart':
+        if k == 'restart':
             self._bailout = True
             raise cherrypy.HTTPRedirect("/")
-        # print('%08x' % self._output)
-        try:
-            self._connect()
-            self._gpio.write_port(self._output)
-        except Exception:
-            self._gpio = False
-        gpios = OrderedDict()
-        for k in range(self.PINS):
-            gpios['_'.join((self.PREFIX, '%d' % k))] = \
-                 (self._output & (1 << k)) and 'checked' or 'unckecked'
+
+        print("FTDI", self._ftdis)
+
+        if k:
+            self._antennas[k] = int(v)
+            ant = int(k.split('_')[0].replace(self.PREFIX, ''))
+
+            try:
+                self._connect()
+                self._ftdis[ant].write_port(self._antennas[k])
+            except Exception as e:
+                import traceback
+                traceback.print_exc()
+                cherrypy.log('FTDI Error: %s (%d MHz)' % (str(e), ant))
+                self._ftdis[ant] = False
+
+        kwargs = {'antennas': OrderedDict()}
+        for band in reversed(sorted(list(self.ANTENNAS))):
+            count, = self.ANTENNAS[band]
+            antname = '%s%d' % (self.PREFIX, band)
+            antenna = kwargs['antennas'][antname] = {}
+            antenna['band'] = band
+            antenna['count'] = list(range(count))
+            antenna['status'] = bool(self._ftdis[band])
+            antenna['sel'] = self._antennas[antname]
+
+        from pprint import pprint
+        pprint(kwargs)
         tmpl = self.env.get_template('index.html')
-        return tmpl.render(gpios=gpios, ftdi=bool(self._gpio))
+        return tmpl.render(**kwargs)
 
     def _connect(self):
-        if self._gpio:
-            return
-        try:
-            gpio = GpioController()
-            gpio.open(vendor=0x403, product=0x6014, interface=1,
-                      direction=(1 << self.PINS) - 1)
-            self._gpio = gpio
-        except Exception as e:
-            cherrypy.log('FTDI Error: %s' % str(e))
+        for ant in self.ANTENNAS:
+            if self._ftdis[ant]:
+                continue
+            try:
+                serial = os.environ.get('FTDI%d' % ant, None)
+                if serial:
+                    gpio = GpioController()
+                    gpio.open(vendor=0x403, product=0x6014, interface=1,
+                              serial=serial, direction=0xFF)
+                    gpio.read_port()
+                else:
+                    gpio = None
+                self._ftdis[ant] = gpio
+                print('New', self._ftdis)
+            except Exception as e:
+                self._ftdis[ant] = False
+                cherrypy.log('FTDI Error: %s (%s)' % (str(e), serial))
 
 
-cherrypy.config.update({'server.socket_host': '0.0.0.0',
-                        'server.socket_port': 8080})
+def main():
+    cherrypy.config.update({'server.socket_host': '0.0.0.0',
+                            'server.socket_port': 8080})
+    cherrypy.quickstart(Root())
 
-cherrypy.quickstart(Root())
+
+if __name__ == '__main__':
+    argparser = ArgumentParser(description=sys.modules[__name__].__doc__)
+    argparser.add_argument('-u', '--ftdi900',
+                           help='FTDI device name for 900 MHz')
+    argparser.add_argument('-w', '--ftdi2450',
+                           help='FTDI device name for 2.45 GHz')
+    args = argparser.parse_args()
+    os.environ['FTDI2450'] = args.ftdi2450 or ''
+    os.environ['FTDI900'] = args.ftdi900 or ''
+    main()
